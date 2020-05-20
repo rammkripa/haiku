7ac047acd27d8cd483edbd0543108212d962286d
Ram Kripa <ramkripa@Rams-Air.attlocal.net>
2020-05-19 17:00:28 -0700

    Initial commit

--- flasky.py
+++ flasky.py
@@ -1,11 +1,11 @@
 # hello world web app
 from flask import Flask, url_for, redirect, request         # import flask
-import shakey as Shakes
+import shakes as Shakey
 app = Flask(__name__)             # create an app instance
 @app.route("/haiku")
 def printy():
 	try:
-		return Shakes.create_random_haiku() 
+		return Shakey.create_random_haiku() 
 	except:
 		return printy()
 

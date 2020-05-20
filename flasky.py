ba09985c97cae51b5883d3039111ee7050ebc533
Ram Kripa <ramkripa@Rams-Air.attlocal.net>
2020-05-19 17:21:40 -0700

    bruh commit

--- flasky.py
+++ flasky.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
-@app.route("/haiku")
+@app.route("/generate")
 def printy():
 	try:
 		return Shakey.create_random_haiku() 
